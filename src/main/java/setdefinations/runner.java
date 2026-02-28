517656254337603f224218aaeaf0f081534715ef
kalya <kalya@yogita.bbrouter>
2026-02-28 10:42:47 +0530

    orangehmr website

--- src/main/java/setdefinations/runner.java
+++ src/main/java/setdefinations/runner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="cucumber_features/addUser.feature",glue="setdefinations")
+@CucumberOptions(features="cucumber_features/login.feature",glue="setdefinations")
 public class runner {
 
 }
